76fe9ee2c71b1f37624c24f2744507a51e634773
Dongliang Yu <yudl.nju@gmail.com>
2014-02-08 16:49:53 +0800

    stopwatch

---
+++ src/algs/Stopwatch.java
@@ -0,0 +1,19 @@
+package algs;
+
+public class Stopwatch {
+	
+	private final long start;
+	
+	public Stopwatch() {
+		start = System.currentTimeMillis();
+	}
+	
+	/**
+	 * elapsed time in seconds 
+	 */
+	public double elapsedTime() {
+		long end = System.currentTimeMillis();
+		return (end - start) / 1000.0;
+	}
+
+}
