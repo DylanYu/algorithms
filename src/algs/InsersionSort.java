62363bfb9a84698e3aa711d2766ff5d8a3755e16
Dongliang Yu <yudl.nju@gmail.com>
2014-02-13 20:58:48 +0800

    try 2 spaces indentation

--- src/algs/InsersionSort.java
+++ src/algs/InsersionSort.java
@@ -2,33 +2,33 @@ package algs;
 
 public class InsersionSort {
 
-	public static void sort(Comparable[] a) {
-		int N = a.length;
-		for (int i = 1; i < N; i++) {
-			for (int j = i; j > 0; j--) {
-				if (less(a[j], a[j - 1]))
-					exch(a, j, j - 1);
-				else
-					break;
-			}
-		}
-	}
+  public static void sort(Comparable[] a) {
+    int N = a.length;
+    for (int i = 1; i < N; i++) {
+      for (int j = i; j > 0; j--) {
+        if (less(a[j], a[j - 1]))
+          exch(a, j, j - 1);
+        else
+          break;
+      }
+    }
+  }
 
-	private static boolean less(Comparable v, Comparable w) {
-		return v.compareTo(w) < 0;
-	}
+  private static boolean less(Comparable v, Comparable w) {
+    return v.compareTo(w) < 0;
+  }
 
-	private static void exch(Comparable[] a, int i, int j) {
-		Comparable tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
-	}
+  private static void exch(Comparable[] a, int i, int j) {
+    Comparable tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
 
-	public static void show(Comparable[] a) {
-		int N = a.length;
-		for (int i = 0; i < N; i++)
-			System.out.print(a[i] + " ");
-		System.out.println();
-	}
+  public static void show(Comparable[] a) {
+    int N = a.length;
+    for (int i = 0; i < N; i++)
+      System.out.print(a[i] + " ");
+    System.out.println();
+  }
 
 }
