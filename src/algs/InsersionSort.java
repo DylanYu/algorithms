892aa08f2d6f56543da0af6db7013d184310f0bd
Dongliang Yu <yudl.nju@gmail.com>
2014-02-18 14:54:18 +0800

    update insersion sort

--- src/algs/InsersionSort.java
+++ src/algs/InsersionSort.java
@@ -8,8 +8,11 @@ package algs;
 public class InsersionSort {
 
     public static void sort(Comparable[] a) {
-        int N = a.length;
-        for (int i = 1; i < N; i++) {
+        sort(a, 0, a.length - 1);
+    }
+    
+    public static void sort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
             for (int j = i; j > 0; j--) {
                 if (less(a[j], a[j - 1]))
                     exch(a, j, j - 1);
