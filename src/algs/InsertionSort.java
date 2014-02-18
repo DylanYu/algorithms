16091bfcd5368e76d4a6be0cdccf8820fdce7932
Dongliang Yu <yudl.nju@gmail.com>
2014-02-18 15:51:09 +0800

    insertion sort bug fix

--- src/algs/InsertionSort.java
+++ src/algs/InsertionSort.java
@@ -13,7 +13,7 @@ public class InsertionSort {
     
     public static void sort(Comparable[] a, int lo, int hi) {
         for (int i = lo; i <= hi; i++) {
-            for (int j = i; j > 0; j--) {
+            for (int j = i; j > lo; j--) {
                 if (less(a[j], a[j - 1]))
                     exch(a, j, j - 1);
                 else
